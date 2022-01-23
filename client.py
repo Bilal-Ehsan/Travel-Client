8314f6337d0d7b02c797aa7c0895aa22191e6a94
Bilal-Ehsan <bilalehsan933@gmail.com>
2022-01-23 18:51:14 +0000

    Implemented function to assign user IDs

--- client.py
+++ client.py
@@ -1,14 +1,18 @@
+import json
+import requests
+
 import colorama
 from colorama import Fore
 
 
 colorama.init(autoreset=True)
 
-id = None
+id = ''
 
 
 def menu():
-    print(f'User ID: {id}\n')
+    print(f'\nUser ID: {Fore.LIGHTCYAN_EX}None\n') if id == '' else print(f'User ID: {Fore.LIGHTCYAN_EX}{id}\n')
+
     print('[1] - Generate user ID')
     print('[2] - Propose a new trip')
     print('[3] - See all proposals')
@@ -17,22 +21,35 @@ def menu():
     print('[6] - Exit system\n')
 
 
+def generate_id():
+    global id
+    if id == '':
+        response = requests.get('http://localhost:8080/api/v1/id/')
+        if response.status_code == 200:
+            response_json = json.loads(response.content)
+            id = response_json['id']
+            print()
+        else:
+            print(f'\n{Fore.LIGHTRED_EX}[ERROR] Could not perform HTTP request!\n')
+    else:
+        print(f'\n{Fore.LIGHTYELLOW_EX}[INFO] You can only generate one ID per session!\n')
+
+
 def main():
-    print(Fore.LIGHTGREEN_EX + 'Welcome to the travel service client!\n')
+    print(Fore.LIGHTGREEN_EX + 'Welcome to the travel service client!')
 
     while True:
         menu()
         try:
             user_input = input('> ')
         except (KeyboardInterrupt, EOFError):
-            print(Fore.LIGHTRED_EX + 'Uh oh... Something unexpected happened. Bye!\n')
+            print(f'\n{Fore.LIGHTRED_EX}[ERROR] Uh oh... Something unexpected happened. Bye!\n')
             break
 
         if user_input == '1':
-            # TODO: Generate a user ID, which is displayed in the menu
-            pass
+            generate_id()
         elif user_input == '6':
-            print(Fore.LIGHTMAGENTA_EX + 'Bye!\n')
+            print(f'\n{Fore.LIGHTMAGENTA_EX}Bye!\n')
             break
 
 
